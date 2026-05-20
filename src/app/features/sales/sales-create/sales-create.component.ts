ba19f271996cb45098812047cef9f35722c5bbf0
Ali Ahsan <aliahsan8751@gmail.com>
2026-05-20 21:25:46 +0500

    initialcreate

---
+++ src/app/features/sales/sales-create/sales-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-create',
+  imports: [],
+  templateUrl: './sales-create.component.html',
+  styleUrl: './sales-create.component.scss'
+})
+export class SalesCreateComponent {
+
+}
